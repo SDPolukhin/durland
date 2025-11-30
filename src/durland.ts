3995da9950fa1172b4b7ecf65bd687dac44dd7ac
Sergey Polukhin <sdpolukhin@edu.hse.ru>
2025-11-30 21:27:06 +0300

    Initial commit

---
+++ src/durland.ts
@@ -0,0 +1,159 @@
+abstract class Modifier {
+    protected abstract readonly __type: string;
+
+    isInstanceOf<T extends Modifier>(type: string): this is T {
+        return this.__type === type;
+    }
+    modifyHealth(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return intermediate;
+    }
+    
+    modifyMoney(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return intermediate;
+    }
+    
+    modifyPsyche(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return intermediate;
+    }
+}
+
+abstract class Durlander extends Modifier {
+    health: number;
+    money: number;
+    psyche: number;
+    history: Step[];
+    
+    constructor() {
+        super();
+        this.health = 10;
+        this.money = 10;
+        this.psyche = 10;
+        this.history = [];
+    }
+}
+
+abstract class Location extends Modifier {
+    num_slesandras: number;
+    num_sisandras: number;
+    num_chuchundras: number;
+    
+    constructor(slesandras: number, sisandras: number, chuchundras: number) {
+        super();
+        this.num_slesandras = slesandras;
+        this.num_sisandras = sisandras;
+        this.num_chuchundras = chuchundras;
+    }
+}
+
+// Конкретные классы действий
+class Zumbalstvo extends Modifier {
+    modifyHealth(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return -1;
+    }
+    
+    modifyMoney(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return 2 * location.num_slesandras;
+    }
+    
+    modifyPsyche(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return -1;
+    }
+}
+
+class Gulbonstvo extends Modifier {
+    modifyHealth(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return -1;
+    }
+    
+    modifyMoney(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return -1;
+    }
+    
+    modifyPsyche(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return 2 * location.num_sisandras;
+    }
+}
+
+class Shlyamsanie extends Modifier {
+    modifyHealth(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return 2 * location.num_chuchundras;
+    }
+    
+    modifyMoney(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return -1;
+    }
+    
+    modifyPsyche(
+        intermediate: number,
+        durlander: Durlander,
+        action: Action,
+        location: Location
+    ): number {
+        return -1;
+    }
+}
+
+class Idle extends Modifier {}
+
+type Action = Zumbalstvo | Gulbonstvo | Shlyamsanie | Idle;
+
+class Step {
+    durlander: Durlander;
+    action: Action;
+    location: Location;
+}
